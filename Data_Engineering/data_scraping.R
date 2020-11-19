6531b28c2b5f66cbe291b288c47da05ccffcfbf5
Chloë Stipinovich <66464858+chloestipinovich@users.noreply.github.com>
2020-11-19 20:23:38 +0200

    Data sraping script with API function

---
+++ Data_Engineering/data_scraping.R
@@ -0,0 +1,35 @@
+
+# Libraries -----------
+{
+  library(request)
+  library(jsonlite)
+  library(httr)
+  library(zoo)
+  library(pastecs)
+  library(data.table)
+  library(corrplot)
+  library(dplyr)
+}
+
+APIrequest = function(){
+  keys     = c("D93BEE3MG1DDV9HFKD4TDZ2B3","PZSMCLY0TV8WDVR85EKPBLMYQ", "94PTQDZ21VSDMHHT2XVFJK40U", "EBAR2HSG4KACLUX2B3E8XEHXU",
+              "YGRJRH8DV62KZ7MAW43SQL900", "EAPTA3QB308L41LWVE35DYEYL", "HA5E9345NW0XENSZCQC5RY4WT")
+  # Forecast --------
+  request_future     = GET(paste("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?location=-33.9345,18.4771&aggregateHours=24&unitGroup=metric&shortColumnNames=false&contentType=json&key=", keys[1], sep = ""))
+  (forecast          = fromJSON(rawToChar(request_future$content))$locations$'-33.9345,18.4771'$values)
+  forecast$datetimeStr = as.Date(forecast$datetimeStr)
+  forecast = forecast[,c("datetimeStr", "mint", "maxt", "humidity", "precip","wspd", "wdir")]
+  forecast = forecast %>% rename(date = datetimeStr, min_temp = mint, max_temp = maxt, humid = humidity, rain = precip, wind_speed = wspd, wind_dir = wdir)
+  # Historical --------
+  startDate         = Sys.Date()-7
+  endDate           = Sys.Date() 
+  request_history   = read.csv(paste("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/history?&aggregateHours=24&startDateTime=", startDate, "&endDateTime=", endDate, 
+                      "&unitGroup=metric&contentType=csv&dayStartTime=0:0:00&dayEndTime=0:0:00&location=-33.9345,18.4771&key=", keys[1], sep = ""))
+  request_history$Date.time = as.Date(request_history$Date.time, format = "%m/%d/%Y")
+  historical = request_history[,c("Date.time", "Minimum.Temperature", "Maximum.Temperature", "Relative.Humidity", "Precipitation", "Wind.Speed", "Wind.Direction")]
+  historical = historical %>% rename(date = Date.time, min_temp = Minimum.Temperature, max_temp = Maximum.Temperature, humid = Relative.Humidity, rain = Precipitation, wind_speed = Wind.Speed, wind_dir = Wind.Direction)
+  # Combined --------
+  return(as.data.frame(rbind(historical[2:8,], forecast[2:8,])))
+}
+
+
