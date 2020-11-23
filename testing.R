b97c91753273eabf49906858defb7576f14b8a56
skycope <skycope@gmail.com>
2020-11-23 11:08:58 +0200

    testing functions

--- testing.R
+++ testing.R
@@ -61,5 +61,74 @@ ema = train %>%
 
 # Here we will need the models
 
+#
+
+# Testing metrics
+
+# Brier score
+test_df = data.frame(
+                     p_verylow = c(0, 0),
+                     p_low = c(0, 0.9),
+                     p_mod = c(0.6, 0),
+                     p_high = c(0.4, 0.1),
+                     p_veryhigh = c(0, 0),
+                     actual_verylow = c(0, 0),
+                     actual_low = c(0, 1),
+                     actual_mod = c(0, 0),
+                     actual_high = c(1, 0),
+                     actual_veryhigh = c(0, 0))
+
+
+brier_score = function(df){
+  brier_score = 0 # set to start at zero
+  for(i in 1:nrow(df)){ # iterate over the rows
+    day_score = 0 # for each day's contribution, start at 0
+    for(j in 1:5){
+      day_score = day_score + (df[i, j] - df[i, j+5])^2
+    }
+    brier_score = brier_score + day_score
+  }
+  return(brier_score/nrow(df))
+}
+
+brier_score(test_df) # seems like it works
+
+# For accuracy and mse
+observed = c(1, 2, 3, 4, 5, 4, 3, 1) %>% as.factor()
+predicted = c(1, 4, 3, 2, 5, 4, 3, 1)  %>% as.factor() 
+
+# Accuracy: use caret on confusion matrix
+library(caret)
+cm = confusionMatrix(observed, predicted)
+cm_table = cm$table
+
+mse = function(cm){
+  mse = 0
+  for(r in 1:5){
+    for(c in 1:5){
+      mse = mse + cm[r, c]* (r - c)^2
+    }
+  }
+  return(mse/sum(cm))
+}
+
+mse(cm_table)
+
+# MAE
+mae = function(cm){
+  mae = 0
+  for(r in 1:5){
+    for(c in 1:5){
+      mae = mae + cm[r, c]* abs(r - c)
+    }
+  }
+  return(mae/sum(cm))
+}
+
+mae(cm_table)
+
+
+
+
 
 
